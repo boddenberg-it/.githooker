6906eb978070221a5b6c7a586102c5daa2ee06fd
André Boddenberg <andre@boddenberg.it>
2020-04-27 03:30:19 +0200

    Change docu and fix relevant shellcheck warnings, TDD is coming

--- run_tests.sh
+++ run_tests.sh
@@ -8,19 +8,18 @@ if [ "$(basename "$BASE")" != "githooks" ]; then
 fi
 
 # sourcing script under test
-source helper.sh
+source "$BASE/do"
 
 function ensure_clean_test_setup {
 	# clean up actual hooks
 	rm -f "$BASE"/githooks/* 
 
 	# clean up all symbolic links
-    for hook in "$BASE"/.git/hooks/*; do
+	for hook in "$BASE"/.git/hooks/*; do
 		if [[ "$hook" != *".sample" ]]; then
 			rm -f "$hook"
 		fi
 	done
-	#echo -e "\n${b}[INFO]${u} test setup cleaned for test of: ${b}$1${u}"
 }
 
 function success {
@@ -29,6 +28,7 @@ function success {
 
 function failure {
 	echo -e "${r}${b}[FAILURE]${d} ${u}$1"
+	final_test_result="1"
 }
 
 function create_hook {
@@ -36,8 +36,8 @@ function create_hook {
 	#	- hook e.g. 'pre-commit', 'post-merge'
 	# $2: 
 	#	- 0 symbolic link in .git/hooks/ (orphaned)
-	#   - 1 creates hook in githooks/	 (disabled)
-	#   - * creates both of the above	 (enabled)
+	#	- 1 creates hook in githooks/ (disabled)
+	#	- * creates both of the above (enabled)
 	
 	hook="$BASE/githooks/$1.ext"
 	link="$BASE/.git/hooks/$1"
@@ -53,23 +53,25 @@ function create_hook {
 }
 
 # create backup of .git/hooks/*
-#hook_backup="$BASE/tests/hook_backup"
-#mkdir -p "$hook_backup"
-#cp "$BASE"/.git/hooks/* "$hook_backup"
+hook_backup="$BASE/tests/hook_backup"
+mkdir -p "$hook_backup"
+cp "$BASE"/.git/hooks/* "$hook_backup"
 
 # begin testing
-echo -e "\n${b}######${u} starting .githooks tests suites ${b}######${u}"
+echo -e "\n${b}######${u} starting .githooks test suites ${b}######${u}"
+final_test_result=0
 
-# LIST
+# LIST TESTS
 echo -e "\n${b}LIST TESTS$u"
+# setup
 ensure_clean_test_setup "list"
 log="$BASE/tests/list.output"
 create_hook pre-commit 0
 create_hook pre-push 1
 create_hook pre-rebase 2
-
+# command under test
 list > log 2>&1
-
+# evaluation
 if grep -q "31mpre-commit" "$log"; then
    	success "list test - finds$r oprhaned$d hook"
 else
@@ -86,12 +88,14 @@ else
 	failure "list test - finds$g enabled$d hook"
 fi
 
+# ENABLE TESTS
 echo -e "\n${b}ENABLE TESTS$u"
 ensure_clean_test_setup "enable one hook"
+# setup
 create_hook pre-commit 1
-
+# command under test
 enable "pre-commit" > /dev/null 2>&1
-
+# evaluation
 if [ -f "$BASE/.git/hooks/pre-commit" ]; then
 	success "enable one hook"
 else
@@ -99,12 +103,13 @@ else
 fi
 
 ensure_clean_test_setup "enable three hooks"
+# setup
 create_hook pre-commit 1
 create_hook pre-push 1
 create_hook pre-rebase 1
-
-enable "pre-commit" "pre-push" "pre-rebase" > /dev/null 2&>1
-
+# command under test
+enable "pre-commit" "pre-push" "pre-rebase" > /dev/null 2>&1
+# evaluation
 if [ -f "$BASE/.git/hooks/pre-commit" ] && [ -f "$BASE/.git/hooks/pre-push" ] && [ -f "$BASE/.git/hooks/pre-rebase" ]; then
 	success "enable three hooks"
 else
@@ -112,12 +117,13 @@ else
 fi
 
 ensure_clean_test_setup "enable --all hooks"
+#setup
 create_hook pre-commit 1
 create_hook pre-push 1
 create_hook pre-rebase 1
-
+# command under test
 enable --alls > /dev/null 2>&1
-
+# evaluation
 if [ -f "$BASE/.git/hooks/pre-commit" ] && [ -f "$BASE/.git/hooks/pre-push" ] && [ -f "$BASE/.git/hooks/pre-rebase" ]; then
 	success "enable --all hooks"
 else
@@ -125,13 +131,14 @@ else
 fi
 
 
-# DISABLE
+# DISABLE TESTS
 echo -e "\n${b}DISABLE TESTS$u"
+# setup
 ensure_clean_test_setup "disable one hook"
 create_hook pre-commit 2
-
-disable "pre-commit" > /dev/null 2&>1
-
+# command under test
+disable "pre-commit" > /dev/null 2>&1
+# evaluation
 if [ -f "$BASE/.git/hooks/pre-commit" ]; then
 	failure "disable one hook"
 else
@@ -139,12 +146,13 @@ else
 fi
 
 ensure_clean_test_setup "disable three hooks"
+# setup
 create_hook pre-commit 2
 create_hook pre-push 2
 create_hook pre-rebase 2
-
-disable "pre-commit" "pre-push" "pre-rebase" > /dev/null 2&>1
-
+# command under test
+disable "pre-commit" "pre-push" "pre-rebase" > /dev/null 2>&1
+# evaluation
 if [ -f "$BASE/.git/hooks/pre-commit" ] || [ -f "$BASE/.git/hooks/pre-push" ] || [ -f "$BASE/.git/hooks/pre-rebase" ]; then
 	failure "disable three hooks"
 else
@@ -152,44 +160,45 @@ else
 fi
 
 ensure_clean_test_setup "disable --all hooks"
+# setup
 create_hook pre-commit 2
 create_hook pre-push 2
 create_hook pre-rebase 2
-
-disable --alls > /dev/null 2&>1
-
+# command under test
+disable --alls > /dev/null 2>&1
+# evaluation
 if [ -f "$BASE/.git/hooks/pre-commit" ] || [ -f "$BASE/.git/hooks/pre-push" ] || [ -f "$BASE/.git/hooks/pre-rebase" ]; then
 	failure "disable --all hooks"
 else
 	success "disable --all hooks"
 fi
 
-# only when expect is installed on system
-expect -v > /dev/null 2&>1
-if [ $? = "0" ]; then
-	
+# INTERACTIVE TESTS (check if expect is available)
+
+if expect -v > /dev/null 2>&1; then
+	echo -e "\n${b}[INFO]$u No expect installation found skipping interactive tests..."
+else
 	echo -e "\n${b}INTERACTIVE TESTS$u"
+	# setup
 	ensure_clean_test_setup "interactive"
 	create_hook pre-commit 0
 	create_hook pre-push 2
 	create_hook pre-rebase 1
-
-	# invoke expect
+	# command under test
 	start="$(date +%s)"
-	expect $BASE/test_interactive.exp "n" > /dev/null 2&>1
+	expect "$BASE/test_interactive.exp" "n" > /dev/null 2>&1
 	end="$(date +%s)"
-	
+	# evaluation (based on time out)
 	if [ $((end-start)) -lt 5 ]; then
 		success "interactive test - answer no to all (smoke test)"
 	else
 		failure "interactive test - answer no to all (smoke test)"
 	fi
-
-	# invoke expect
+	# command under test
 	start="$(date +%s)"
-	expect $BASE/test_interactive.exp "y" > /dev/null 2&>1
+	expect "$BASE/test_interactive.exp" "y" > /dev/null 2>&1
 	end="$(date +%s)"
-	
+	# evaluations:
 	if [ $((end-start)) = 5 ]; then
 		failure "interactive test - answer yes to all"
 	else
@@ -206,17 +215,17 @@ if [ $? = "0" ]; then
 			success "interactive test - disable enabled hook"
 		fi
 		# check if pre-rebase is enabled 
-		if [ ! -f $BASE/.git/hooks/pre-rebases ]; then
+		if [ ! -f "$BASE/.git/hooks/pre-rebases" ]; then
 			failure "interactive test - enable disabled hook"
 		else
 			success "interactive test - enable disabled hook"
 		fi
 	fi
-else
-	echo "\n$b[INFO]$u No expect installation found skipping interactive tests..."
 fi
+
 # restoring old hooks and deleting backup
 ensure_clean_test_setup
-#cp "$hook_backup"/* "$BASE"/.git/hooks/
-#rm -rf "$hook_backup"
+cp "$hook_backup"/* "$BASE"/.git/hooks/
+rm -rf "$hook_backup"
 echo
+exit $final_test_result
