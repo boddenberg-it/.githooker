8e6c59acffbf3e759a4d64639c8269081efe665e
André Boddenberg <andre@boddenberg.it>
2020-04-29 23:09:49 +0200

    fix path

--- tests/travis.sh
+++ tests/travis.sh
@@ -2,4 +2,4 @@
 
 git config --global user.name "githooker"
 git config --global user.email "githooker@boddenberg.it"
-docker build -f "$PWD/test/Dockerfile.$distro" .
+docker build -f "$PWD/tests/Dockerfile.$distro" .
