a560b524e05de4f4643e9e7fb70605b066514674
André Boddenberg <andre@boddenberg.it>
2020-04-30 19:35:53 +0200

    Debug commit - to be ammended

--- generic_hooks.sh
+++ generic_hooks.sh
@@ -3,19 +3,23 @@
 b="\x1B[1m"  # bold
 u="\x1B[0m"  # unbold
 
+exit_code=0
+
 function run_command_for_each_file {
-	for changed_file in $(ls); do
+	for changed_file in $(git diff HEAD --name-only --cached); do
 		if [ $(echo "$changed_file" | grep -c -e "${1//\,/\\}") = 1 ]; then
 			$2 "$changed_file"
+			exit_code=$((exit_code+$?))
 		fi
 	done
 }
 
 function run_command_once {
-	for changed_file in $(ls); do
+	for changed_file in $(git diff HEAD --name-only --cached); do
 		if [ $(echo "$changed_file" | grep -c -e "${1//\,/\\}") = 1 ]; then
 			$2
-			break
+			exit_code=$((exit_code+$?))
+			continue
 		fi
 	done
 }
