0f68052e7e1b66e4943ca6e6865125684d8b6a0a
André Boddenberg <andre@boddenberg.it>
2020-05-02 02:19:13 +0200

    Remove unnecessary error handling

--- generic_hooks.sh
+++ generic_hooks.sh
@@ -3,13 +3,10 @@
 b="\x1B[1m"  # bold
 u="\x1B[0m"  # unbold
 
-exit_code=0
-
 function run_command_for_each_file {
 	for changed_file in $(git diff HEAD --name-only --cached); do
 		if [ $(echo "$changed_file" | grep -c -e "${1//\,/\\|}") = 1 ]; then
 			$2 "$changed_file"
-			exit_code=$((exit_code+$?))
 		fi
 	done
 }
@@ -18,8 +15,6 @@ function run_command_once {
 	for changed_file in $(git diff HEAD --name-only --cached); do
 		if [ $(echo "$changed_file" | grep -c -e "${1//\,/\\|}") = 1 ]; then
 			$2
-			exit_code=$((exit_code+$?))
-			continue
 		fi
 	done
 }
