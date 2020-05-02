22d4f2ebd4609478b91c63080d20ea7258954d3e
André Boddenberg <andre@boddenberg.it>
2020-05-02 16:27:50 +0200

    Add docker test image for centOS and alpine and run them in travis

--- tests/test_dockerfiles.sh
+++ tests/test_dockerfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-distros=(debian ubuntu archlinux)
+distros=(alpine archlinux centos debian ubuntu)
 
 for distro in ${distros[@]}; do
     docker build --no-cache -t "githooker_$distro" -f "$PWD/tests/_docker/Dockerfile.$distro" .
