588a91eb5e33d02f3232435309d76253aa6a6802
André Boddenberg <andre@boddenberg.it>
2020-05-04 23:14:46 +0200

    Remove unnecessary semicolons at end of commands + fix typo

--- tests/_docker/run_githooker_testsuites.sh
+++ tests/_docker/run_githooker_testsuites.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # some distros do not seem to like github cert
-git config --global http.sslVerify false ;\
+git config --global http.sslVerify false
 # initial git setup - git commit fails elsewise
-git config --global user.email "githooker@boddenberg.it" ;\
-git config --global user.name "githooker" ;\
+git config --global user.email "githooker@boddenberg.it"
+git config --global user.name "githooker"
 
 # run .githooker testsuites dev setup
 git clone https://github.com/boddenberg-it/.githooker.git 
 cd .githooker && ./tests/run_tests.sh
 cd ..
 
-# run .githookertestsutes in super project
+# run .githooker testsutes in super project
 git clone https://github.com/boddenberg-it/croni && cd croni
 git submodule add https://github.com/boddenberg-it/.githooker .githooker
 .githooker/test --skip-prompt
