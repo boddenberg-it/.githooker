c12cee3017d1fd4b28a8faf8dbda14ac9a422856
André Boddenberg <andre@boddenberg.it>
2020-05-04 23:29:54 +0200

    Fix typos in comments

--- tests/_docker/run_githooker_testsuites.sh
+++ tests/_docker/run_githooker_testsuites.sh
@@ -2,16 +2,17 @@
 
 # some distros do not seem to like github cert
 git config --global http.sslVerify false
+
 # initial git setup - git commit fails elsewise
 git config --global user.email "githooker@boddenberg.it"
 git config --global user.name "githooker"
 
-# run .githooker testsuites dev setup
+# run .githooker testsuites in dev setup
 git clone https://github.com/boddenberg-it/.githooker.git 
 cd .githooker && ./tests/run_tests.sh
 cd ..
 
-# run .githooker testsutes in super project
+# run .githooker testsuites in super project
 git clone https://github.com/boddenberg-it/croni && cd croni
 git submodule add https://github.com/boddenberg-it/.githooker .githooker
 .githooker/test --skip-prompt
