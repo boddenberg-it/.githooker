2d6f9178765e03665ee1934320ee31c2529f0eeb
André Boddenberg <andre@boddenberg.it>
2020-05-08 12:50:45 +0200

    Add several tests cases for enable/disable
    Now every test case runs for enable and disable. Furthermore,
    following test cases have been added for both commands:
    
    - passing hook without extension
    
    Disable command got an extra test case:
    
    - does not remove actual file in $GIT_HOOK_DIR

--- tests/pass_hook_path_with_extension_test.sh
+++ tests/pass_hook_path_with_extension_test.sh
@@ -1,17 +1,54 @@
 ensure_clean_test_setup "passing path to hook script"
 disabled_hook pre-commit
 disabled_hook pre-push
+touch "$hook_dir/pre-applypatch"
 
-enable ".githook/pre-commit.sh" "pre-push.sh" > /dev/null
+enable ".githook/pre-commit.sh" "pre-push.ext" "pre-applypatch" > /dev/null
 
 if [ -L "$BASE/$GIT_HOOK_DIR/pre-commit" ]; then
-	success "EN/DISable - passing path to hook script"
+	success "Enable - passing path to hook script"
 else
-	failure "EN/DISable - passing path to hook script"
+	failure "Enable - passing path to hook script"
 fi
 
 if [  -L "$BASE/$GIT_HOOK_DIR/pre-push" ]; then
-	success "EN/DISable - passing hook with extension"
+	success "Enable - passing hook with extension"
 else
-	failure  "EN/DISable - passing hook with extension"
+	failure  "Enable - passing hook with extension"
+fi
+
+if [  -L "$BASE/$GIT_HOOK_DIR/pre-applypatch" ]; then
+	success "Enable - passing hook without extension"
+else
+	failure  "Enable - passing hook without extension"
+fi
+
+
+disable ".githook/pre-commit.sh" "pre-push.ext" "pre-applypatch" > /dev/null
+
+if [ ! -L "$BASE/$GIT_HOOK_DIR/pre-commit" ]; then
+	success "Disable - passing path to hook script"
+else
+	failure "Disable - passing path to hook script"
+fi
+
+if [ ! -L "$BASE/$GIT_HOOK_DIR/pre-push" ]; then
+	success "Disable - passing hook with extension"
+else
+	failure  "Disable - passing hook with extension"
+fi
+
+if [ ! -L "$BASE/$GIT_HOOK_DIR/pre-applypatch" ]; then
+	success "Disable - passing hook without extension"
+else
+	failure  "Disable - passing hook without extension"
+fi
+
+touch $BASE/$GIT_HOOK_DIR/pre-commit
+disable "pre-commit" > /dev/null
+
+if [ -f  "$BASE/$GIT_HOOK_DIR/pre-commit" ]; then
+	success "Disable - does not remove actual file in $GIT_HOOK_DIR/"
+else
+	failure  "Disable  - does not remove actual file in $GIT_HOOK_DIR"
 fi
