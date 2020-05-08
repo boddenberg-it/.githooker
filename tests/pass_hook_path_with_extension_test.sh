3ac726adb0735ac1b3d34adb04733de7c0e3cbe0
André Boddenberg <andre@boddenberg.it>
2020-05-08 12:58:26 +0200

    Align lower case for report lines

--- tests/pass_hook_path_with_extension_test.sh
+++ tests/pass_hook_path_with_extension_test.sh
@@ -6,49 +6,49 @@ touch "$hook_dir/pre-applypatch"
 enable ".githook/pre-commit.sh" "pre-push.ext" "pre-applypatch" > /dev/null
 
 if [ -L "$BASE/$GIT_HOOK_DIR/pre-commit" ]; then
-	success "Enable - passing path to hook script"
+	success "enable - passing path to hook script"
 else
-	failure "Enable - passing path to hook script"
+	failure "enable - passing path to hook script"
 fi
 
 if [  -L "$BASE/$GIT_HOOK_DIR/pre-push" ]; then
-	success "Enable - passing hook with extension"
+	success "enable - passing hook with extension"
 else
-	failure  "Enable - passing hook with extension"
+	failure  "enable - passing hook with extension"
 fi
 
 if [  -L "$BASE/$GIT_HOOK_DIR/pre-applypatch" ]; then
-	success "Enable - passing hook without extension"
+	success "enable - passing hook without extension"
 else
-	failure  "Enable - passing hook without extension"
+	failure  "enable - passing hook without extension"
 fi
 
 
 disable ".githook/pre-commit.sh" "pre-push.ext" "pre-applypatch" > /dev/null
 
 if [ ! -L "$BASE/$GIT_HOOK_DIR/pre-commit" ]; then
-	success "Disable - passing path to hook script"
+	success "disable - passing path to hook script"
 else
-	failure "Disable - passing path to hook script"
+	failure "disable - passing path to hook script"
 fi
 
 if [ ! -L "$BASE/$GIT_HOOK_DIR/pre-push" ]; then
-	success "Disable - passing hook with extension"
+	success "disable - passing hook with extension"
 else
-	failure  "Disable - passing hook with extension"
+	failure  "disable - passing hook with extension"
 fi
 
 if [ ! -L "$BASE/$GIT_HOOK_DIR/pre-applypatch" ]; then
-	success "Disable - passing hook without extension"
+	success "disable - passing hook without extension"
 else
-	failure  "Disable - passing hook without extension"
+	failure  "disable - passing hook without extension"
 fi
 
 touch $BASE/$GIT_HOOK_DIR/pre-commit
 disable "pre-commit" > /dev/null
 
 if [ -f  "$BASE/$GIT_HOOK_DIR/pre-commit" ]; then
-	success "Disable - does not remove actual file in $GIT_HOOK_DIR/"
+	success "disable - does not remove actual file in $GIT_HOOK_DIR/"
 else
-	failure  "Disable  - does not remove actual file in $GIT_HOOK_DIR"
+	failure  "disable  - does not remove actual file in $GIT_HOOK_DIR"
 fi
